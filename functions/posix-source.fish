213bcf57e2d8126e79411472deb33fb242db1c47
T.S. Foster <hi@tsf.io>
2018-06-26 15:28:07 +0100

    Add no-global and unexport flags

--- functions/posix-source.fish
+++ functions/posix-source.fish
@@ -2,7 +2,9 @@ function posix-source --description='Source env files'
   set -l options \
     (fish_opt --short=h --long=help) \
     (fish_opt --short=p --long=print) \
-    (fish_opt --short=n --long=no-interpret)
+    (fish_opt --short=n --long=no-interpret) \
+    (fish_opt --short=G --long=no-global) \
+    (fish_opt --short=u --long=unexport)
   argparse $options -- $argv
 
   [ (count $argv ) -eq 0 ]; and set _flag_help
@@ -24,12 +26,26 @@ function posix-source --description='Source env files'
       	-p, --print
       		Print the commands to STDOUT
 
+      	-G, --no-global
+      		By default, variables will be set globally. This flag causes
+      		variables to be unset at the end of the block.
+
+      	-u, --unexport
+      		By default, variables will be exported to child processes.
+      		This flag prevents that.
+
       	-n, --no-interpret
       		By default, variables in env files will be interpreted. Pass
       		this argument to interpret dollar signs as literal characters." \
     | string replace --all --regex '(^ +)' ''
     return 0
   end
+
+  set -q _flag_no_global
+  or set setFlags -g
+
+  set -q _flag_unexport
+  or set setFlags -x $setFlags
 
 
   for file in $argv
@@ -41,12 +57,12 @@ function posix-source --description='Source env files'
       set arr (string split -m1 = $instruction)
 
       set -q _flag_print
-      and echo set -gx $arr[1] $arr[2]\;
+      and echo set $setFlags $arr[1] $arr[2]\;
       and continue
 
       set -q _flag_no_interpret
-      and set -gx $arr[1] $arr[2]
-      or eval "set -gx $arr[1] $arr[2]"
+      and set $setFlags $arr[1] $arr[2]
+      or eval "set $setFlags $arr[1] $arr[2]"
     end
   end
 
